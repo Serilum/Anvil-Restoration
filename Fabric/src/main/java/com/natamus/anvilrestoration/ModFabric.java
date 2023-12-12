1ea314dcfe3d1e272bb5bd5b793874ebe2682c1d
Rick South <rick@natamus.com>
2023-12-12 11:05:01 +0100

    Anvil Restoration source code for 1.18.2.

---
+++ Fabric/src/main/java/com/natamus/anvilrestoration/ModFabric.java
@@ -0,0 +1,30 @@
+package com.natamus.anvilrestoration;
+
+import com.natamus.anvilrestoration.events.AnvilInteractEvent;
+import com.natamus.anvilrestoration.util.Reference;
+import com.natamus.collective.check.RegisterMod;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+			return AnvilInteractEvent.onAnvilClick(player, world, hand, hitResult);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
