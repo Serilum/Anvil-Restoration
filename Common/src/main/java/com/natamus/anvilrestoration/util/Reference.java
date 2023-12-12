96fce3c2fd1f3059fa0bc4d5ca4ac9eee9c29101
Rick South <rick@natamus.com>
2023-12-12 11:05:10 +0100

    Anvil Restoration source code for 1.19.3.

---
+++ Common/src/main/java/com/natamus/anvilrestoration/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.anvilrestoration.util;
+
+public class Reference {
+	public static final String MOD_ID = "anvilrestoration";
+	public static final String NAME = "Anvil Restoration";
+	public static final String VERSION = "2.0";
+	public static final String ACCEPTED_VERSIONS = "[1.19.3]";
+}
