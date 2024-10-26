6037e2048766a1beb1bb42e474a7bad5e94ca79e
Rick South <rick@natamus.com>
2024-10-26 15:25:34 +0200

    Anvil Restoration source code for 1.21.3.

---
+++ Forge/src/main/java/com/natamus/anvilrestoration/forge/events/ForgeAnvilInteractEvent.java
@@ -0,0 +1,17 @@
+package com.natamus.anvilrestoration.forge.events;
+
+import com.natamus.anvilrestoration.events.AnvilInteractEvent;
+import net.minecraft.world.InteractionResult;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeAnvilInteractEvent {
+	@SubscribeEvent
+	public void onAnvilClick(PlayerInteractEvent.RightClickBlock e) {
+		if (AnvilInteractEvent.onAnvilClick(e.getEntity(), e.getLevel(), e.getHand(), e.getHitVec()).equals(InteractionResult.SUCCESS)) {
+			e.setCanceled(true);
+		}
+	}
+}
